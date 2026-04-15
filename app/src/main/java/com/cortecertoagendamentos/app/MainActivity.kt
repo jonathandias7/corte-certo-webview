c9b797ceec24da6284e5e6beb88f770e46fb585c
jonathandias7 <126732186+jonathandias7@users.noreply.github.com>
2026-04-15 18:13:39 -0300

    Update MainActivity.kt

--- app/src/main/java/com/cortecertoagendamentos/app/MainActivity.kt
+++ app/src/main/java/com/cortecertoagendamentos/app/MainActivity.kt
@@ -1,1 +1,39 @@
+package com.cortecertoagendamentos.app
 
+import android.os.Bundle
+import android.webkit.*
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        webView = WebView(this)
+        setContentView(webView)
+
+        val settings = webView.settings
+        settings.javaScriptEnabled = true
+        settings.domStorageEnabled = true
+        settings.loadWithOverviewMode = true
+        settings.useWideViewPort = true
+        settings.allowFileAccess = true
+        settings.allowContentAccess = true
+        settings.mixedContentMode = WebSettings.MIXED_CONTENT_ALWAYS_ALLOW
+
+        webView.webViewClient = WebViewClient()
+        webView.webChromeClient = WebChromeClient()
+
+        webView.loadUrl("https://cortecertoagendamentos.com.br/")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
