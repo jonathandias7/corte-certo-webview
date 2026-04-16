beaafd1229cdf51d2f0768891dbcced32c83105f
jonathandias7 <126732186+jonathandias7@users.noreply.github.com>
2026-04-15 23:59:08 -0300

    Update MainActivity.kt

--- app/src/main/java/com/cortecertoagendamentos/app/MainActivity.kt
+++ app/src/main/java/com/cortecertoagendamentos/app/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.cortecertoagendamentos.app
 
 import android.annotation.SuppressLint
+import android.net.http.SslError
 import android.os.Build
 import android.os.Bundle
 import android.webkit.*
+import androidx.activity.OnBackPressedCallback
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -26,40 +28,39 @@ class MainActivity : AppCompatActivity() {
         settings.allowFileAccess = true
         settings.allowContentAccess = true
 
-        // 🔥 SEGURANÇA CORRETA
+        // 🔥 SEGURANÇA
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             settings.mixedContentMode = WebSettings.MIXED_CONTENT_NEVER_ALLOW
         }
 
-        // 🔥 PERFORMANCE
         settings.cacheMode = WebSettings.LOAD_DEFAULT
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             settings.safeBrowsingEnabled = true
         }
 
-        // 🔥 HARDWARE ACCELERATION
         webView.setLayerType(WebView.LAYER_TYPE_HARDWARE, null)
 
         webView.webViewClient = object : WebViewClient() {
 
-            override fun shouldOverrideUrlLoading(view: WebView?, request: WebResourceRequest?): Boolean {
+            override fun shouldOverrideUrlLoading(
+                view: WebView?,
+                request: WebResourceRequest?
+            ): Boolean {
                 request?.url?.let {
                     view?.loadUrl(it.toString())
                 }
                 return true
             }
 
-            // 🔥 SSL SEGURO (IMPORTANTE PRA PLAY STORE)
             override fun onReceivedSslError(
                 view: WebView?,
                 handler: SslErrorHandler,
                 error: SslError?
             ) {
-                handler.cancel() // NÃO IGNORA ERRO
+                handler.cancel() // segurança Play Store
             }
 
-            // 🔥 ERRO DE CONEXÃO
             override fun onReceivedError(
                 view: WebView?,
                 request: WebResourceRequest?,
@@ -75,16 +76,18 @@ class MainActivity : AppCompatActivity() {
 
         webView.webChromeClient = WebChromeClient()
 
-        // 🔥 SUA URL
         webView.loadUrl("https://cortecertoagendamentos.com.br/")
-    }
 
-    override fun onBackPressed() {
-        if (this::webView.isInitialized && webView.canGoBack()) {
-            webView.goBack()
-        } else {
-            super.onBackPressed()
-        }
+        // 🔥 NOVO BACK PRESS (ANDROID MODERNO)
+        onBackPressedDispatcher.addCallback(this, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (webView.canGoBack()) {
+                    webView.goBack()
+                } else {
+                    finish()
+                }
+            }
+        })
     }
 
     override fun onDestroy() {
